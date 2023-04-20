a663d4b134986757daf42c04411e1c773173c8fd
Laia <laia.m.penalva@gmail.com>
2023-04-20 19:59:00 +0200

    Subida del ejercicio 0

---
+++ crud-ejemplo/src/main/java/com/hibernate/App.java
@@ -0,0 +1,38 @@
+package com.hibernate;
+
+import java.util.List;
+
+import com.hibernate.dao.PersonaDAO;
+import com.hibernate.model.Persona;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		PersonaDAO personaDAO= new PersonaDAO();
+		
+		Persona pers1=new Persona ("Juan", 19);
+		personaDAO.insertPersona(pers1);
+		
+		Persona pers2=new Persona ("Ana", 63);
+		personaDAO.insertPersona(pers2);
+		
+		Persona pers3=new Persona ("Juana", 24);
+		personaDAO.insertPersona(pers3);
+
+		pers1.setEdad(40);
+		personaDAO.updatePersona(pers1);
+		
+		Persona pers4=personaDAO.selectPersonaById(2);
+		System.out.println(pers4.getNombre());
+		
+		List<Persona> personas=personaDAO.selectAllPerson();
+		
+		for (Persona p: personas) {
+			System.out.println(p.getNombre() + " " + p.getEdad());
+		}
+		
+		personaDAO.deletePersona(3);
+	}
+
+}
